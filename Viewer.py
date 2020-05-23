0721effa08758257b0994f4c3526649ea9c4e341
Mfon Udoh <mu257@bath.ac.uk>
2020-05-23 19:36:55 +0100

    viewer class and buttons

--- Viewer.py
+++ Viewer.py
@@ -1,88 +1,130 @@
 import tkinter as tk
 
-window = tk.Tk()
-window.title("Transport Network")
-window.columnconfigure(0, weight=1, minsize=800)
-window.rowconfigure(1, weight=1, minsize=500)
+class GUI(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+        self.grid()
+        self.master.title("Transport Network")
 
-btnPadding = 5
+        self.columnconfigure(0, weight=1, minsize=800)
+        self.rowconfigure(1, weight=1, minsize=500)
 
-fr_topbtns = tk.Frame(window, bg="black")
-fr_topbtns.grid(row=0, column=0, sticky="ew")
-btn_open = tk.Button(fr_topbtns, text="Open", fg="white", highlightbackground="black")
-btn_save = tk.Button(fr_topbtns, text="Save", fg="white", highlightbackground="black")
-btn_run = tk.Button(fr_topbtns, text="Run", fg="white", highlightbackground="black")
-btn_open.grid(row=0, column=1, sticky="ew", padx=btnPadding, pady=btnPadding)
-btn_save.grid(row=0, column=2, sticky="ew", pady=btnPadding)
-btn_run.grid(row=0, column=3, sticky="ew", padx=btnPadding, pady=btnPadding)
+        self.topBar = tk.Frame(
+            self, bg="black"
+        )
+        self.topBar.grid(row=0, column=0, sticky="ew")
 
-fr_main = tk.Frame(window)
-fr_main.grid(row=1, column=0, sticky="nsew")
-fr_main.columnconfigure(1, weight=1)
-fr_main.rowconfigure(0, weight=1)
+        self.mainWindow = tk.Frame(self)
+        self.mainWindow.grid(row=1, column=0, sticky="nesw")
+        self.mainWindow.columnconfigure(1, weight=1, minsize=700)
+        self.mainWindow.rowconfigure(0, weight=1)
 
-fr_sidebtns = tk.Frame(fr_main, bg="black")
-fr_sidebtns.grid(row=0, column=0, sticky="ns")
+        self.sideBar = tk.Frame(
+            self.mainWindow, bg="black"
+        )
+        self.sideBar.grid(row=0, column=0, sticky="ns")
 
-def increment(var, j):
-    vars = {
-        "hub"   :   [lbl_hub_val, 2, 15],
-        "dep"   :   [lbl_dep_val, 1, 10],
-        "con"   :   [lbl_con_val, 1, 50]
-    }
-    i = int(vars[var][0]["text"])
-    if i+j <= vars[var][2] and i+j >= vars[var][1]:
-        vars[var][0]["text"] = i+j
+        self.map = tk.Frame(
+            self.mainWindow, bg="green"
+        )
+        self.map.grid(row=0, column=1, sticky="nsew")
+        self.map.columnconfigure([x for x in range(10)], weight=1)
+        self.map.rowconfigure([x for x in range(10)], weight=1)
 
-fr_hub = tk.Frame(fr_sidebtns, bg="black")
-fr_hub.grid(row=0, column=0, sticky="ew")
-fr_hub.columnconfigure(0, weight=1)
-fr_hub.rowconfigure(0, weight=1)
-fr_hubBtn = tk.Frame(fr_hub, bg="black")
-fr_hubBtn.grid(row=1, column=0)
+        self.option_buttons()
+        self.vars()
 
-lbl_hub = tk.Label(fr_hub, text="Hubs", fg="white", bg="black")
-btn_hub_in = tk.Button(fr_hubBtn, text="+", fg="white", highlightbackground="black", command=lambda: increment("hub", 1))
-lbl_hub_val = tk.Label(fr_hubBtn, text="10", fg="white", bg="black")
-btn_hub_dec = tk.Button(fr_hubBtn, text="-", fg="white", highlightbackground="black", command=lambda: increment("hub", -1))
-lbl_hub.grid(row=0, column=0, padx=btnPadding, pady=btnPadding)
-btn_hub_in.grid(row=0, column=2, sticky="ew", padx=btnPadding, pady=btnPadding)
-lbl_hub_val.grid(row=0, column=1, padx=btnPadding, pady=btnPadding)
-btn_hub_dec.grid(row=0, column=0, sticky="ew", padx=btnPadding, pady=btnPadding)
+        self.mainloop()
 
-fr_dep = tk.Frame(fr_sidebtns, bg="black")
-fr_dep.grid(row=1, column=0, sticky="ew")
-fr_dep.columnconfigure(0, weight=1)
-fr_dep.rowconfigure(0, weight=1)
-fr_depBtn = tk.Frame(fr_dep, bg="black")
-fr_depBtn.grid(row=1, column=0)
+    def option_buttons(self):
+        xPadding = 5
+        yPadding = 3
 
-lbl_dep = tk.Label(fr_dep, text="Depots", fg="white", bg="black")
-btn_dep_in = tk.Button(fr_depBtn, text="+", fg="white", highlightbackground="black", command=lambda: increment("dep", 1))
-lbl_dep_val = tk.Label(fr_depBtn, text="5", fg="white", bg="black")
-btn_dep_dec = tk.Button(fr_depBtn, text="-", fg="white", highlightbackground="black", command=lambda: increment("dep", -1))
-lbl_dep.grid(row=0, column=0, padx=btnPadding, pady=btnPadding)
-btn_dep_in.grid(row=0, column=2, sticky="ew", padx=btnPadding, pady=btnPadding)
-lbl_dep_val.grid(row=0, column=1, padx=btnPadding, pady=btnPadding)
-btn_dep_dec.grid(row=0, column=0, sticky="ew", padx=btnPadding, pady=btnPadding)
+        btn_open = tk.Button(
+            self.topBar, text="Open", 
+            fg="white", highlightbackground="black"
+            ).grid(row=0, column=0, padx=xPadding, pady=yPadding)
 
-fr_con = tk.Frame(fr_sidebtns, bg="black")
-fr_con.grid(row=2, column=0, sticky="ew")
-fr_con.columnconfigure(0, weight=1)
-fr_con.rowconfigure(0, weight=1)
-fr_conBtn = tk.Frame(fr_con, bg="black")
-fr_conBtn.grid(row=1, column=0)
+        btn_save = tk.Button(
+            self.topBar, text="Save",
+            fg="white", highlightbackground="black"
+            ).grid(row=0, column=1, padx=xPadding, pady=yPadding)
 
-lbl_con = tk.Label(fr_con, text="Consignments", fg="white", bg="black")
-btn_con_in = tk.Button(fr_conBtn, text="+", fg="white", highlightbackground="black", command=lambda: increment("con", 1))
-lbl_con_val = tk.Label(fr_conBtn, text="20", fg="white", bg="black")
-btn_con_dec = tk.Button(fr_conBtn, text="-", fg="white", highlightbackground="black", command=lambda: increment("con", -1))
-lbl_con.grid(row=0, column=0, padx=btnPadding, pady=btnPadding)
-btn_con_in.grid(row=0, column=2, sticky="ew", padx=btnPadding, pady=btnPadding)
-lbl_con_val.grid(row=0, column=1, padx=btnPadding, pady=btnPadding)
-btn_con_dec.grid(row=0, column=0, sticky="ew", padx=btnPadding, pady=btnPadding)
+        btn_run = tk.Button(
+            self.topBar, text="Run", 
+            fg="white", highlightbackground="black", command=self.start_sim
+            ).grid(row=0, column=2, padx=xPadding, pady=yPadding)
 
-fr_map = tk.Label(fr_main, bg="green")
-fr_map.grid(row=0, column=1, sticky="nsew")
+        btn_stop = tk.Button(
+            self.topBar, text="Stop", 
+            fg="white", highlightbackground="black"
+            ).grid(row=0, column=3, padx=xPadding, pady=yPadding)
 
-window.mainloop()
+        btn_reset = tk.Button(
+            self.topBar, text="Reset", 
+            fg="white", highlightbackground="black"
+            ).grid(row=0, column=4, padx=xPadding, pady=yPadding)
+
+    def vars(self):
+        self.vars = {}
+        vars = {
+            "hub"   :   ["Hub", 10],
+            "dep"   :   ["Depot", 5],
+            "con"   :   ["Consignment", 20]
+        }
+
+        def var_hub(vars, var, row):
+            yPadding = 5
+            xPadding = 3
+
+            var_frame = tk.Frame(self.sideBar, bg="black")
+            var_frame.grid(row=row, column=0, sticky="ew", padx=xPadding, pady=yPadding)
+            var_frame.columnconfigure(0, weight=1)
+            var_frame.rowconfigure(0, weight=1)
+
+            var_frameTop = tk.Frame(var_frame, bg="black")
+            var_frameTop.grid(row=0, column=0)
+
+            tk.Label(
+                var_frameTop, 
+                text="{}s".format(vars[var][0]),
+                fg="white", bg="black"
+            ).grid(row=0, column=0, sticky="ew")
+
+            var_frameBottom = tk.Frame(var_frame, bg="black")
+            var_frameBottom.grid(row=1, column=0)
+
+            lbl_val = tk.Label(
+                var_frameBottom, 
+                text="{}".format(vars[var][1]),
+                fg="white", bg="black"
+            )
+            lbl_val.grid(row=0, column=1, sticky="ew")
+            self.vars[var] = lbl_val["text"]
+
+            def increment(var, j):
+                vars = {
+                "hub"   :   [2, 15],
+                "dep"   :   [1, 10],
+                "con"   :   [1, 50]
+                }
+                i = int(lbl_val["text"])
+                if i+j <= vars[var][1] and i+j >= vars[var][0]:
+                    lbl_val["text"] = i+j
+                    self.vars[var] = lbl_val["text"]
+
+            tk.Button(var_frameBottom, text="+", command=lambda: increment(var, 1),
+            fg="white", highlightbackground="black").grid(row=0, column=2)
+            tk.Button(var_frameBottom, text="-", command=lambda: increment(var, -1),
+            fg="white", highlightbackground="black").grid(row=0, column=0)
+        
+        i = 0
+        for v in vars:
+            var_hub(vars, v, i)
+            i += 1
+
+    def start_sim(self):
+        hub = tk.Frame(self.map, bg="blue", width=20, height=20)
+        hub.grid(row=self.vars['hub'], column=self.vars['dep'])
+
+GUI()
